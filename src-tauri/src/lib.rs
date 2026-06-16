7577e604a1ca932dc198b4977d7349139234716a
winmac <mail>
2026-06-16 09:33:55 +0800

    feat(tauri): add signed updater support

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1273,6 +1273,8 @@ pub fn run() {
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![
             get_all_assistants,
             get_ai_list,
